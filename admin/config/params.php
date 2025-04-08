dbce748c12df7da933ceb5f1bc231c1349532b62
KevDom0317 <156467209+KevDom0317@users.noreply.github.com>
2025-04-08 09:30:34 -0600

    Cambios
    Cambios

--- admin/config/params.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'adminEmail' => 'samuel.flores@aitelecom.net',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
-	'npag' => 20,
-];
--- admin/test.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo cal_days_in_month(CAL_GREGORIAN, date('m', strtotime(date('Y-m')." -1 month")), date('Y', strtotime(date('Y-m')." -1 month")));
-?>
